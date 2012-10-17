899cbac58efa365bebf7b56296e27f376a2c4d64
Jorrit Poelen <jhpoelen@gmail.com>
2012-10-17 13:18:04 -0700

    added taxon uri based on matches found in NCBI, ITIS or WoRMS taxnonomies.  If no match is found, wikipedia is used

--- app/models/specimen.rb
+++ app/models/specimen.rb
@@ -2,13 +2,13 @@ class Specimen
 
   require 'net/http'
 
-  attr_accessor :latitude, :longitude, :length_in_mm, :species, :thumbnail
+  attr_accessor :latitude, :longitude, :length_in_mm, :species, :speciesExternalId, :thumbnail,:taxonUri
   
   def self.fetch_specimens(lat, lon)
     p "lat: #{lat} lon: #{lon}"
     query = "START location=node:locations('*:*') 
-             MATCH location<-[:COLLECTED_AT]-specimen-[:CLASSIFIED_AS]->species 
-             WHERE location.latitude=#{lat} AND location.longitude=#{lon} RETURN specimen, species"
+    MATCH location<-[:COLLECTED_AT]-specimen-[:CLASSIFIED_AS]->species 
+    WHERE location.latitude=#{lat} AND location.longitude=#{lon} RETURN specimen, species"
     uri = URI(Settings.neo4j_service)
     response = Net::HTTP.post_form(uri, 'query' => query)
     body = JSON.parse response.body
@@ -19,6 +19,8 @@ class Specimen
       specimen.longitude = lon
       specimen.length_in_mm = dat[0]['data']['lengthInMm']
       specimen.species = dat[1]['data']['name']
+      specimen.speciesExternalId = dat[1]['data']['externalId']
+      fetch_taxonUri(specimen)
       # fetch_thumbnail(specimen)
       specimens << specimen
     end
@@ -27,12 +29,35 @@ class Specimen
   
   private 
   
-    def self.fetch_thumbnail(specimen)
-      species = specimen.species  
-      sp = RDFS::Resource.new("http://dbpedia.org/resource/#{species}")
-      specimen.thumbnail = sp.dbpedia::thumbnail.uri
-      p "#{specimen.thumbnail}"      
-    rescue
+  def self.fetch_taxonUri(specimen) 
+    speciesExternalId = specimen.speciesExternalId
+    if speciesExternalId
+      if speciesExternalId.starts_with?("NCBI")
+        speciesId = speciesExternalId.split(":")[1]
+        specimen.taxonUri = "http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="
+        specimen.taxonUri << speciesId
+      elsif speciesExternalId.start_with?("urn:lsid:marinespecies.org:taxname")
+       speciesId = speciesExternalId.split(":")[4]
+       specimen.taxonUri = "http://www.marinespecies.org/aphia.php?p=taxdetails&id="
+       specimen.taxonUri << speciesId
+     elsif speciesExternalId.start_with?("urn:lsid:itis.gov:itis_tsn")
+      speciesId = speciesExternalId.split(":")[4]
+      specimen.taxonUri = "http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value="
+      specimen.taxonUri << speciesId
+    end
+  else
+      #a wild guess
+      specimen.taxonUri = "http://www.wikipedia.org/wiki/"
+      specimen.taxonUri << specimen.species.split(" ")[0]
+  end 
+end
+
+def self.fetch_thumbnail(specimen)
+  species = specimen.species  
+  sp = RDFS::Resource.new("http://dbpedia.org/resource/#{species}")
+  specimen.thumbnail = sp.dbpedia::thumbnail.uri
+  p "#{specimen.thumbnail}"      
+rescue
       #fetch_thumbnail_second_try(specimen)
     end
     
@@ -47,5 +72,5 @@ class Specimen
       @@no_image_available.push species[0]
       p "no thumbnail for #{species[0]}"
     end
-  
-end
+
+  end
