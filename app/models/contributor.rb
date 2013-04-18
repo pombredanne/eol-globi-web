06589704ff410bb5f8f93282cd2959a0c822e476
Jorrit Poelen <jhpoelen@gmail.com>
2013-04-18 12:59:43 -0700

    added rest config

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -7,13 +7,9 @@ class Contributor
 	:timePeriod,:numberOfInteractions,:numberOfPreyTaxa
 
 	def self.fetch_contributors
-		query = "START study=node:studies('*:*')
-		MATCH study-[:COLLECTED]->sourceSpecimen-[interact:ATE|PREYS_UPON|PARASITE_OF|HAS_HOST|INTERACTS_WITH]->prey-[:CLASSIFIED_AS]->targetTaxon, sourceSpecimen-[:CLASSIFIED_AS]->sourceTaxon
-		RETURN study.institution, study.period, study.description, study.contributor, count(interact), count(distinct(sourceTaxon)), count(distinct(targetTaxon))"
-		
-		body = executeQuery(query)
+		body = executeQuery
 
-    contributors = Array.new
+		contributors = Array.new
 		body['data'].each do |dat| 
 			p dat
 			contributor = Contributor.new
@@ -21,18 +17,18 @@ class Contributor
 			contributor.timePeriod = dat[1]
 			contributor.studyTitle = dat[2]
 			contributor.name = dat[3]
-      contributor.numberOfInteractions = dat[4]
-      contributor.numberOfPredatorSpecies = dat[5]
-      contributor.numberOfPreyTaxa = dat[6]
-      contributors << contributor
+			contributor.numberOfInteractions = dat[4]
+			contributor.numberOfPredatorSpecies = dat[5]
+			contributor.numberOfPreyTaxa = dat[6]
+			contributors << contributor
 		end
 
 		contributors
 	end
 
-	def self.executeQuery(query)
-		uri = URI(Settings.neo4j_service)
-		response = Net::HTTP.post_form(uri, 'query' => query)
+	def self.executeQuery
+		uri = URI(Settings.globi_rest_service + 'contributors')
+		response = Net::HTTP.get_response(uri)
 		body = JSON.parse response.body
 	end
 
