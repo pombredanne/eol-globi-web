559916b2060a51e371422cad1f7be58e5a5fa40b
reiz <robert.reiz.81@gmail.com>
2013-04-22 23:48:06 +0200

    add travis.yml file

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper'
 
 describe Location do
-
-  before(:each) do
-    @location = Location.new({:latitude => "9999", :longitude => "4444"})
-    @location.save
-  end
-  
-  after(:each) do 
-    @location.remove
-  end
   
   describe "fetch_locations" do 
     
